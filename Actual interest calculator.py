5f29045ba0d9d0809c1fd9e15069bafdadee055e
Shankhesh-16 <67553761+Shankhesh-16@users.noreply.github.com>
2020-07-07 10:45:48 +0530

    Update Actual interest calculator.py

--- Actual interest calculator.py
+++ Actual interest calculator.py
@@ -1,11 +1,10 @@
 def calculator(amount,taxrate,roi,inflation,years):
     compount_interest =  amount * (((1 + roi/100)**years) - 1)
     after_deduct_tax = compount_interest - (compount_interest * taxrate/100)
-    total_amount = round(amount + after_deduct_tax , 2)
+    total_amount = amount + after_deduct_tax
     after_deduct_inflation = total_amount - (total_amount * inflation/100)
-    final_amount = after_deduct_inflation
+    final_amount = round(after_deduct_inflation,2)
 
-    print(total_amount)
     print("After deducting tax and inflation* your investment value would be " + str(final_amount) + " at the rate of " + str(roi) + "% compunded " + str(years) + " times.")
     print("* - inflation is deducted on total amount")
 
